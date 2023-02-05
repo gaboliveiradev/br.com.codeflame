59842f0e8384c9adede7d80fb5514ac365bf70fc
gaboliveiradev <g38789138@gmail.com>
2023-02-05 08:13:40 -0300

    first commit

---
+++ View/modules/welcome/index.view.php
@@ -0,0 +1,52 @@
+﻿<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./../../../View/css/welcome/index.style.css">
+    <link rel="shortcut icon" href="./../../../View/assets/favicon.png" type="image/x-icon">
+    <title>Codeflame - The New Framework For PHP</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="./../../../View/assets/logo.png">
+        </div>
+        <div class="nav">
+            <ul>
+                <li><a href="">About</a></li>
+                <li><a href="">Documentation</a></li>
+                <li><a href="">Realeases</a></li>
+                <li><a href="">Pay My Coffee</a></li>
+                <li><a href="">Github</a></li>
+            </ul>
+        </div>
+        <div class="follow">
+            <a target="__blank" href="https://www.instagram.com/eugabriieloliveira_/">Follow The Developer</a>
+        </div>
+    </header>
+    <main>
+        <div class="box-main">
+            <div class="title">
+                <h1>Transform your development with Codeflame <span class="destaque">the revolutionary PHP framework.</span></h1>
+            </div>
+            <div class="subtitle">
+                <p>CodeFlame is an excellent choice for PHP developers seeking to increase their productivity and efficiency without sacrificing code quality or application functionality. Try it today and experience the difference that a well-designed framework can make in your software development journey!</p>
+            </div>
+            <div class="buttons">
+                <div class="getstarted">
+                    <a href="">GET STARTED</a>
+                </div>
+                <div class="documentation">
+                    <a href="">DOCUMENTATION</a>
+                </div>
+            </div>
+        </div>
+
+        <img class="img1" src="./../../../View/assets/favicon.png">
+        <img class="img2" src="./../../../View/assets/favicon.png">
+        <img class="img3" src="./../../../View/assets/favicon.png">
+    </main>
+</body>
+</html>
