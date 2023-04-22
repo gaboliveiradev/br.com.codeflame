8a8cba5481e566dbf84bc6acf5c18390b30e944c
gaboliveiradev <g38789138@gmail.com>
2023-04-22 08:54:41 -0300

    finalizando a sidebar de sumario da página de documentação

--- View/modules/documentation/index.view.php
+++ View/modules/documentation/index.view.php
@@ -14,5 +14,101 @@
         <?php include "./View/includes/navbar.php" ?>
     </header>
     
+    <section class="main">
+        <section class="sidebar__sumario">
+            <div class="title__sumario__documentation">
+                <h3><i class="bi bi-file-earmark-code-fill"></i> Sumário Documentação</h3>
+            </div>
+            <div class="itens__sumario">
+                <ul>
+                    <li>
+                        <h4>Arquivos Raiz</h4>
+                        <ol>
+                            <li>
+                                <a href="">Arquivo indedx.php</a>
+                            </li>
+                            <li>
+                                <a href="">Arquivo config.php</a>
+                            </li>
+                            <li>
+                                <a href="">Arquivo autoload.php</a>
+                            </li>
+                            <li>
+                                <a href="">Arquivo rotas.php</a>
+                            </li>
+                        </ol>
+                    </li>
+                    <li>
+                        <h4>Pasta View</h4>
+                        <ol>
+                            <li>
+                                <a href="">Pasta "assets"</a>
+                            </li>
+                            <li>
+                                <a href="">Pasta "bootstrap"</a>
+                            </li>
+                            <li>
+                                <a href="">Pasta "css"</a>
+                            </li>
+                            <li>
+                                <a href="">Pasta "includes"</a>
+                            </li>
+                            <li>
+                                <a href="">Pasta "modules"</a>
+                            </li>
+                        </ol>
+                    </li>
+                    <li>
+                        <h4>Pasta Controller</h4>
+                        <ol>
+                            <li>
+                                <a href="">Classe Abstrata Controller.php</a>
+                            </li>
+                        </ol>
+                    </li>
+                    <li>
+                        <h4>Pasta Model</h4>
+                        <ol>
+                            <li>
+                                <a href="">Classe Abstrata Model.php</a>
+                            </li>
+                        </ol>
+                    </li>
+                    <li>
+                        <h4>Pasta DAO</h4>
+                        <ol>
+                            <li>
+                                <a href="">Classe Abstrata DAO.php</a>
+                            </li>
+                        </ol>
+                    </li>
+
+                    <li>
+                        <h4>Tópicos</h4>
+                        <ol>
+                            <li>
+                                <a href="">Criar nova rota</a>
+                            </li>
+                            <li>
+                                <a href="">Dados de conexão MySQL</a>
+                            </li>
+                            <li>
+                                <a href="">Alterar SGBD</a>
+                            </li>
+                            <li>
+                                <a href="">Autenticação de login</a>
+                            </li>
+                            <li>
+                                <a href="">Padrão de Projeto MVC</a>
+                            </li>
+                        </ol>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <section class="content__documentation">
+            -- aqui vai o conteudo da documentação --
+        </section>
+    </section>
 </body>
 </html>
